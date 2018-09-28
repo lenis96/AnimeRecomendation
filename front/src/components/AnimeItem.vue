c3a39e336b0e3c0900602d172e9e49e53475fe0f
lenis96 <lenisandres5@gmail.com>
2018-09-28 14:59:54 -0500

    creadi AnimeItem

---
+++ front/src/components/AnimeItem.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <img :src="image" alt="">
+    <li>{{name}}</li>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        name:String
+    },
+    name:'animeItem',
+    data(){
+        return {
+
+            image:"https://i.pximg.net/img-original/img/2018/09/25/10/58/11/70864741_p0.jpg"
+        }
+    }
+}
+</script>
+
+<style>
+    img{
+        width:200px;
+        /* height: 100px; */
+    }
+</style>
+
--- front/src/components/AnimeList.vue
+++ front/src/components/AnimeList.vue
@@ -1,12 +1,18 @@
 <template>
+<div>
+
     <ul>
-        <li v-for="anime in animes" :key="anime.name">{{anime.name}}</li>
+        <!-- <li v-for="anime in animes" :key="anime.name">{{anime.name}}</li> -->
+        <AnimeItem :name="anime.name" v-for="anime in animes" :key="anime.name"/>
     </ul>
+</div>
 </template>
 
 <script>
+import AnimeItem from './AnimeItem'
 export default {
     name:'AnimeList',
+    components:{AnimeItem},
     data(){
         return {
             animes:[
